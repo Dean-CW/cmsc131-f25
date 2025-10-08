c9f95d08fb44f0f56be7e6372cc99c60e69dc661
John M Dusel <jdusel@frederick.edu>
2025-10-08 18:57:37 +0000

    FBK: Account class
    - missing account type
    
    - missing data validation in constructor

--- src/projects/bank/Account.java
+++ src/projects/bank/Account.java
@@ -4,9 +4,12 @@ public class Account {
     private String id;
     private String ownerName;
     private double balance;
-    private String accountName;
+    private String accountName; // TODO remove
+    // TODO add account type
 
     public Account(String tempId, double tempBalance, String tempAccountName) {
+        // TODO data validation
+        // TODO account type
         id = tempId;
         balance = tempBalance;
         accountName = tempAccountName;
